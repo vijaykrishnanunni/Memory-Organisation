03b8de5810774bafca2721579be5d860d4a8741d
vijay <vijaykrishnanunni11@gmail.com>
2026-03-03 15:13:13 +0530

    Add files via upload

---
+++ prototype/design_0_wrapper.v
@@ -0,0 +1,19 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2024.2 (lin64) Build 5239630 Fri Nov 08 22:34:34 MST 2024
+//Date        : Tue Mar  3 14:28:26 2026
+//Host        : vijay-ASUS-TUF running 64-bit Ubuntu 24.04.2 LTS
+//Command     : generate_target design_0_wrapper.bd
+//Design      : design_0_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_0_wrapper
+   ();
+
+
+  design_0 design_0_i
+       ();
+endmodule
